37b8e5be2404f546db59bf9690da3dcf40ceba75
Fredy <fredzuna@hotmail.com>
2024-03-28 17:12:32 -0400

    fix resolver path

--- src/helpers/createSchema.ts
+++ src/helpers/createSchema.ts
@@ -1,10 +1,11 @@
 import { GraphQLSchema } from 'graphql';
 import { BuildSchemaOptions, buildSchemaSync } from 'type-graphql';
 import { Container } from 'typedi';
+import config from '../config';
 
 export function createSchema(options?: Omit<BuildSchemaOptions, 'resolvers'>): GraphQLSchema {
-  return buildSchemaSync({
-    resolvers: [`${__dirname}/../resolvers/**/*.ts`],
+  return buildSchemaSync({    
+    resolvers: config.resolvers,
     emitSchemaFile: true,
     container: Container,
     validate: true,
