2ae2e2969d0ec9f2ccc35b6fb18de0001ef2abf8
Fredy <fredzuna@hotmail.com>
2024-04-05 23:17:54 -0400

    configure sqlite

--- src/config/development.ts
+++ src/config/development.ts
@@ -3,13 +3,12 @@ import ConfigInterface from './ConfigInterface';
 const config: ConfigInterface = {
   env: 'development',
   database: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'portfoliodb',
-    entities: ['src/entities/*.ts'],    
+    type: 'sqlite' as const,
+    cache: false,
+    database: 'database.db',
+    entities: ['src/entities/*.ts'],
+    logger: 'advanced-console' as const,
+    synchronize: false,
     migrations: ['src/migrations/*.ts'], // Array of migration files (can be js/ts)
     cli: {
       entitiesDir: 'src/entities',
--- src/config/test.ts
+++ src/config/test.ts
@@ -3,18 +3,13 @@ import ConfigInterface from './ConfigInterface';
 const config: ConfigInterface = {
   env: 'test',
   database: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'portfoliodb',
-    entities: ['src/entities/*.ts'],    
-    migrations: ['src/migrations/*.ts'], // Array of migration files (can be js/ts)
-    cli: {
-      entitiesDir: 'src/entities',
-      migrationsDir: 'src/migrations',
-    },
+    type: 'sqlite' as const,
+    cache: false,
+    database: ':memory:',
+    dropSchema: true,
+    entities: ['src/entities/*.ts'],
+    logger: 'advanced-console' as const,
+    synchronize: true,
   },
   graphQLPath: '/graphql',
   resolvers: [`${__dirname}/../resolvers/**/*Resolver.ts`],
